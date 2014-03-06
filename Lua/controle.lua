25116fa7cf56e9c4ccdb0ec963ec8ec33ac8ffef
Luciana Sena <luciana.aguiar.sena@gmail.com>
2014-03-06 18:25:52 -0400

    Controle.lua

--- Lua/controle.lua
+++ Lua/controle.lua
@@ -1,13 +1,10 @@
 local yellowmarker_p = canvas:new('media/markeryellow_p.jpg')
+local redmarker_p = canvas:new('media/markerred_p.jpg')
 
-local redmarker = canvas:new('media/markerred.jpg')
-local yellowmarker = canvas:new('media/markeryellow.jpg')
+local manaus = yellowmarker_p
 
-local manaus = yellowmarker
-local selectmanaus = canvas:new('media/markeryellow.jpg')
-
-local rio = canvas:new('media/markerred.jpg')
-local selectrio = canvas:new('media/markeryellow.jpg')
+local rio = canvas:new('media/markerred_p.jpg')
+local selectrio = canvas:new('media/markeryellow_p.jpg')
 
 local salvador = canvas:new('media/markerred.jpg')
 local selectsalvador = canvas:new('media/markeryellow.jpg')
@@ -15,7 +12,7 @@ local selectsalvador = canvas:new('media/markeryellow.jpg')
 canvas:attrColor(0, 0, 0, 0)
 canvas:drawRect('fill', 10, 10, canvas:attrSize())
 
-canvas:compose(10, 10, manaus)
+canvas:compose(100, 100, manaus)
 canvas:compose(30, 30, rio)
 canvas:compose(70, 70, salvador)
 
@@ -31,7 +28,7 @@ function redesenhar()
 end
 
 function controleHandler (evt)
-  if evt.class == 'key' and evt.type == 'press' and evt.key == 'CURSOR_RIGHT' and manaus == yellowmarker then
+  if manaus == yellowmarker then
     redesenhar(manaus, rio)
   end
 
