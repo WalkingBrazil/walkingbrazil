e594aebd3891ef796a95dc0f20fa609601b678a0
Flavio Ipiranga <flavio.ipiranga@gmail.com>
2014-03-11 17:00:14 -0400

    Atualização telaSelecao.ncl e selecao.lua Implementado a questão do lua alterar o nome da cidade de acordo com a cidade selecionada. Implementado a transição de contexto do main.ncl para o telaSelecao.ncl Implementado navegação dos pontos turísticos.

---
+++ Lua/selecao.lua
@@ -0,0 +1,19 @@
+require 'manaus'
+
+local top_cinco = (1080*5)/100
+local left_cinco = (1920*5)/100
+
+function handler (evt)
+
+  if evt.type == 'attribution' then
+    local cidade = evt.value
+    canvas:attrFont('vera', 35)
+    canvas:attrColor('blue')
+    canvas:drawText (left_cinco, top_cinco, cidade)
+    local teste = 'Descricao breve de ' .. cidade ..  ' fica aqui!!!'
+    canvas:drawText (12*left_cinco, top_cinco, teste)
+    canvas:drawText (12*left_cinco, 5.5*top_cinco, 'Selecione o seu ponto turistico abaixo:')
+  end
+
+end
+event.register(handler)
